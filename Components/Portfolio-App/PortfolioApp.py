4fbdbfa7834a36ba76d9ff8e09f781b54d44ce7b
carpacker <carpacker@vassar.edu>
2018-05-29 13:27:58 -0400

    Started frontend

--- Components/Portfolio-App/PortfolioApp.py
+++ Components/Portfolio-App/PortfolioApp.py
@@ -19,7 +19,6 @@ import Siamining
 # Contains widgets, screens and functions for building a front end
 #  portfolio management system.
 
-# TODO:
 #   - Consistent sidebar/bottom
 #   - Side/bottom bar has a few options
 #    - Mining information
@@ -32,111 +31,25 @@ import Siamining
 class MainScreen(Screen):
     fullscreen = BooleanProperty(False)
 
-        def add_widget(self, *args):
-    if 'content' in self.ids:
-        return self.ids.content.add_widget(*args)
-    return super(ShowcaseScreen, self).add_widget(*args)
+    def add_widget(self, *args):
+        if 'content' in self.ids:
+            return self.ids.content.add_widget(*args)
+        return super(ShowcaseScreen, self).add_widget(*args)
 
     # FUNCTION: updateTime
     # DESCRIPTION:
     #   Updates time in the corner of the screen 
-    def 
-    pass
+    def updateTime():
+        pass
 
-# SCREEN: Mining
+
+    # Gridlayout populated with buttons that move to other screens
+
+# SCREEN: FundManagement
 # DESCRIPTION:
-#   Main screen that displays all pertinent mining information.
-class MiningScreen(Screen):
-    def main(address):
-        # STATIC variables 
-        # - Address : address of worker, used to grab other files
-        # - Worker_count : # of intended workers
-
-        MiningScreen.initializeScreens()
-        ticker = 0
-        while 1:
-        # UPDATED EVERY 5 MINUTES
-            ticker += 1
-            if ticker == 600:
-                # Retrieve values from API
-                global_valuesE = MiningScreen.updateGlobals()
-                global_valuesS = MiningScreen.updateGlobals()
-
-                worker_one = MiningScreen.updateWorker()
-                worker_two = MiningScreen.updateWorker()
-                worker_three = MiningScreen.updateWorker()
-                worker_four = MiningScreen.updateWorker()
-                worker_five = MiningScreen.updateWorker()
-                worker_six = MiningScreen.updateWorker()
-
-                front_end_tuple = (0)
-                updateFrontEnd(front_end_tuple)
-
-                # Probably need an error checking system
-
-            time.sleep(1)
-
-    # FUNCTION: initializeStaticVariables
-    def initializeStaticVariables():
-        # Intended Worker Count
-        worker_count = 7
-
-        # Mining Addresses
-        eth_address = ""
-        sc_address = ""
-        dcr_address = ""
-        zec_address = ""
-
-        worker_one_name = ""
-        worker_two_name = ""
-        worker_three_name = ""
-        worker_four_name = ""
-        worker_five_name = ""
-        worker_six_name = ""
-        worker_seven_name = ""
-
-        worker_one_type = "NVIDIA"
-        worker_two_type = "NVIDIA"
-        worker_three_type = "AMD"
-        worker_four_type = "AMD"
-        worker_five_type = "AMD"
-        worker_six_type = "AMD"
-        worker_seven_type = "AMD"
-
-        worker_one_mining = "ETHSC"
-        worker_two_mining = "ZEC"
-        worker_three_mining = "ETHSC"
-        worker_four_mining = "ETHSC"
-        worker_five_mining = "ETHSC"
-        worker_six_mining = "ETHSC"
-        worker_seven_mining = "ETHSC"
-
-        # Set KV variables to the above
-
-    # FUNCTION: updateWorker
-    def updateWorker():
-        # Update INDIVIDUAL worker values
-
-        # 1. Grab values from API
-        current_hashrate_eth = Ethermine.getCurrentHash()
-        current_hashrate_sc = Siamining.getCurrentHash()
-
-        reported_hashrate_eth = Ethermine.getReportedHash()
-        reported_hashrate_sc = Siamining.getReportedHash()
-
-        average_hashrate_eth = Ethermine.getAverageHash()
-        average_hashrate_sc = Siamining.getAverageHash()
-
-    # FUNCTION: updateGlobals
-    def updateGlobals():
-        # Update GLOBAL values
-
-        # 1. Grab values from API
-        network_eth = Ethermine.getNetworkDifficulty()
-        network_sc = Siamining.getNetworkDifficulty()
-
-        worker_count_eth = Ethermine.getWorkerCount()
-        worker_count_sc = Siamining.getWorkerCount
+#   Provides tools to manage the funds on various exchanges
+class FundManagementScreen(Screen):
+    pass
 
 # SCREEN: MarketInformation
 # DESCRIPTION:
@@ -171,17 +84,119 @@ class MarketInfoScreen(Screen):
         # Submit above to alert bot, alert bot deals with accordingly
         pass
 
-class CoinListing(GridLayout):
-    def __init___(self, **kwargs):
-        super(CoinListingself).__init__(**kwargs)
-    pass
-
-# SCREEN: FundManagement
+    class CoinListing(GridLayout):
+        def __init___(self, **kwargs):
+            super(CoinListingself).__init__(**kwargs)
+        pass
+# SCREEN: Mining
 # DESCRIPTION:
-#   Provides tools to manage the funds on various exchanges
-class FundManagementScreen(Screen):
-    pass
-    
+#   Main screen that displays all pertinent mining information.
+class MiningScreen(Screen):
+
+    # ON ENTER
+
+    # ON PAUSE
+
+    # ON STOP
+
+    # Top-level function
+    def main(address):
+        # STATIC variables 
+        # - Address : address of worker, used to grab other files
+        # - Worker_count : # of intended workers
+
+        MiningScreen.initializeScreens()
+        ticker = 0
+        while 1:
+        # UPDATED EVERY 5 MINUTES
+            ticker += 1
+            if ticker == 600:
+                # Retrieve values from API
+                global_valuesE = MiningScreen.updateGlobals()
+                global_valuesS = MiningScreen.updateGlobals()
+
+                worker_one = MiningScreen.updateWorker()
+                worker_two = MiningScreen.updateWorker()
+                worker_three = MiningScreen.updateWorker()
+                worker_four = MiningScreen.updateWorker()
+                worker_five = MiningScreen.updateWorker()
+                worker_six = MiningScreen.updateWorker()
+
+                front_end_tuple = (0)
+                updateFrontEnd(front_end_tuple)
+
+                # Probably need an error checking system
+
+            time.sleep(1)
+
+    # FUNCTION: initializeStaticVariables
+    # DESCRIPTION:
+    #   Function responsible for initializing the static variables.
+    def initializeStaticVariables(self):
+        # Intended Worker Count
+        worker_count = 7
+
+        # Mining Addresses
+        eth_address = ""
+        sc_address = ""
+        dcr_address = ""
+        zec_address = ""
+
+        worker_one_name = "NV1-1"
+        worker_two_name = "NV2-2"
+        worker_three_name = "AMD1-3"
+        worker_four_name = "AMD2-4"
+        worker_five_name = "AMD3-5"
+        worker_six_name = "AMD4-6"
+        worker_seven_name = "AMD5-7"
+
+        worker_one_type = "NVIDIA"
+        worker_two_type = "NVIDIA"
+        worker_three_type = "AMD"
+        worker_four_type = "AMD"
+        worker_five_type = "AMD"
+        worker_six_type = "AMD"
+        worker_seven_type = "AMD"
+
+        worker_one_mining = "ETHSC"
+        worker_two_mining = "ZEC"
+        worker_three_mining = "ETHSC"
+        worker_four_mining = "ETHSC"
+        worker_five_mining = "ETHSC"
+        worker_six_mining = "ETHSC"
+        worker_seven_mining = "ETHSC"
+
+        # Set KV variables to the above
+
+    # FUNCTION: updateWorker
+    # DESCRIPTION:
+    #   Responsible for updating the dynamic worker values.
+    def updateWorker():
+        # Update INDIVIDUAL worker values
+
+        # 1. Grab values from API
+        current_hashrate_eth = Ethermine.getCurrentHash()
+        current_hashrate_sc = Siamining.getCurrentHash()
+
+        reported_hashrate_eth = Ethermine.getReportedHash()
+        reported_hashrate_sc = Siamining.getReportedHash()
+
+        average_hashrate_eth = Ethermine.getAverageHash()
+        average_hashrate_sc = Siamining.getAverageHash()
+
+    # FUNCTION: updateGlobals
+    # DESCRIPTION:
+    #   Responsible for updating dynamic global values.
+    def updateGlobals():
+        # Update GLOBAL values
+
+        # 1. Grab values from API
+        network_eth = Ethermine.getNetworkDifficulty()
+        network_sc = Siamining.getNetworkDifficulty()
+
+        worker_count_eth = Ethermine.getWorkerCount()
+        worker_count_sc = Siamining.getWorkerCount
+
 # Settings [Screen] 
 #   Accessible from every screen. Contains settings that can be adjusted globally 
 #    and per screen.
@@ -231,9 +246,10 @@ class PortfolioApp(App):
         self.current_title = screen.name
         self.update_sourcecode()
 
-    # TODO:
-    #   Pull other functions as necessary from samples
 
-if __name__ == '__main__':
-    PortfolioApp().run()
+
+buildKV = Builder.load_file("Display.kv")
+if __name__ == "__main__":
+    PorfolioApp().run()
+        
 
