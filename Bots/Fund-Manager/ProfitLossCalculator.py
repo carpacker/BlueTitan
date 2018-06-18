d6398234a12e63a33f47b13d78f4395eeb7a5b9c
Carson Packer <carsongpacker@gmail.com>
2018-06-18 15:17:37 -0400

    Worked in calculateFIFOProfit; file will be deprecated in next commit

--- Bots/Fund-Manager/ProfitLossCalculator.py
+++ Bots/Fund-Manager/ProfitLossCalculator.py
@@ -17,6 +17,12 @@ import PrintLibrary
 #   Container for suite of functions that process the transactions across various exchanges.
 class TransactionProcesser(Object):
 
+    # FUNCTION: main
+    # INPUT: exchanges - [string, ...]
+    # OUTPUT: N/A
+    # DESCRIPTION:
+    #   Top level function for converting a list of buys and sells into a FIFO
+    #    profit loss generator
     def main(self):
         # 1. Build the transaction CSV in chronological order
         chronological_txs = self.buildTxCSV(exchanges, "time")
@@ -72,15 +78,90 @@ class TransactionProcesser(Object):
             print("Error, not supported :order_by: input")
             return -1
 
+        
         def processTransactions():
-            pass
+            # Label withdrawals as either SELL or TRANSFER
+            # Use deposit addresses to be able to tell
 
-        def calculateFIFOprofit():
-            pass
+        
+        def calculateFIFOprofit(transactions):
+            inputs = []
+            outputs = []
+            
+            for row in transactions:
+                if row[1] == 'buy':
+                    inputs.append(row)
+                elif row[1] == 'sell':
+                    outputs.append(row) 
+                
+            # 2. While there are outputs still left to be acted over, calculate
+            #     profit loss using FIFO methodology.
+            running_profit = 0
+            running_loss = 0
+
+            total_in = 0
+            total_out = 0
+
+            ctr_flag = 2
+            
+            while len(outputs) > 0:
+
+                # Control flag determines what elements to pop:
+                # 2 - both
+                # 1 - inputs
+                # 0 - outputs
+                if ctr_flag == 2:
+                    current_output = outputs.pop()
+                    current_input = inputs.pop()
+
+                elif ctr_flag == 1:
+                    current_input = inputs.pop()
+
+                elif ctr_flag == 0:
+                    current_output = outputs.pop()
+                
+                print("---- ITERATION " + str(len(outputs)) + "----")
+                print("OUT:", outputs)
+                print("IN:", inputs)
+                print("CURRENT PROFIT:", running_profit)
+            
+                # CASE: Sell is larger - work through buys
+                curr_value = float(current_output[2])
+                print(current_input, current_output)
+                while curr_value > float(current_input[2]):
+                    # Calculate the profit FILO
+                    orig_value = float(current_input[4]) * float(current_input[5])
+                    sell_value = float(current_input[4]) * float(current_output[5])
+                    profit_loss = sell_value - orig_value
+
+                    # Add to running profit, adjust current output's value for next iteration
+                    running_profit += profit_loss
+                    current_output[4] = float(current_output[4]) - float(current_input[4])
+                    curr_value = float(current_output[2]) - sell_value
+                    print(orig_value,sell_value,profit_loss,running_profit,curr_value)
+
+                    # Set control flag to pop input
+                    current_input = inputs.pop()
+                    time.sleep(3)
+
+                 # CASE: Buy is larger, continue loop 
+                 orig_value = float(current_output[4]) * float(current_input[5])
+                 sell_value = float(current_output[4]) * float(current_output[5])
+                 profit_loss = sell_value - orig_value
+
+                # Add to running profit, adjust current output's value for next iteration
+                running_profit += profit_loss
+                current_input[4] = float(current_input[4]) - float(current_output[4])
+                print(orig_value,sell_value,profit_loss,running_profit)
+
+                # Pop new input
+                ctr_flag = 0
+                time.sleep(3)           
 
         def buildFinalCSV():
-            pass
-        
+            # 1. Build any derivative data
+            # 2. Create and write the actual CSV file
+            
         # FUNCTION: buildExchangeAddresses
         # INPUT: exchanges - [string, ...]
         #        assets    - [string, ...]
