9e5c609eab7b91db609ff1b36a3b7bbd40d5228d
Carson Packer <carsongpacker@gmail.com>
2018-06-22 02:49:48 -0400

    Cleaned up CoinCategorizer and its readme

--- Bots/Coin-Categorizer/CoinCategorizer.py
+++ Bots/Coin-Categorizer/CoinCategorizer.py
@@ -1,40 +1,47 @@
-import sys
+# CoinCategorizer.py
+# Carson Packer
 
+# External-Imports
+import sys
+import csv
+
+# Internal-Import
 from API import ExchangeAPI
 
-# Mass autobuy script
-# Purpose:
-#   Iterate through a list of cryptocurrencies with defined attributes, give
-#   them an aggregate  score based on these attributes, identify coins whichare
-#   'to be bought' and then go through
-#   the API of whatever sites are necessary to buy them.
+# CLASS: CoinCategorizer
+# DESCRIPTION:
+#    Suite of functions relevant to cateogrizing and purchasing unknown cryptocurrencies.
+def CoinCategorizer(Object):
 
+    # FUNCTION: main
+    # INPUT: assets - list of supported assets
+    # OUTPUT: N/A
+    # DESCRIPTION: 
+    #   Top level function for the coin categorizer.
+    def main(file_name):
+        # 1. Parsse the CSV for the values we will be working with
+        coin_info = CoinCategorizer.parseCSV(file_name)
+        balances = getBalances(asset)
+        coin_list = rankCoins()
+        distribution_values = allocateFunds(coin_list, balances)
+        
+    # FUNCTION: parseCSV
+    # INPUT: csv_name - string
+    # OUTPUT: [(data, ...), ...]
+    # DESCRIPTION:
+    #    Parses a CSV file and turns it into an effective matrix (list of tuples).
+    # NOTE: probably move this to helpers or something
+    def parseCSV():
+        pass
 
-# CLASS: SpreadsheetParser
-class SpreadsheetParser():
-
-    spreadsheet_dict = {}
-
-    def processCSV():
-        currency_list = []
-
-        # 1. Access the file
-        # 2. for each line
-        #   - Store values in temporary tuple
-        #   - Append tuple to running list
-        #   - Store tuple in database
-
-        return currency_list
-
-    # OUTPUT: DICTIONARY
-    #     [Asset :  (PRICE, VOLUME, MARKETS, TIMELINE, SOCIAL_NEWS, WEBSITE,
-    #                   SOURCE_CODE, PRICE_MOV, MINEABLE, WHITEPAPER)]  
-    #     [ETH   :   (8, 10, 8, ...)] --> 8
-    # LENGTH = (1 + NUMBER OF VARIABLES)#
-
-#CLASS: CoinRanker
-class CoinRanker():
-
+    # FUNCTION: buildAssetList
+    # INPUT: coin_info - [(data, ...), ...]
+    # OUTPUT: list
+    # DESCRIPTION:
+    #    Runs through our coin info list taking each asset and adding it to asset list.
+    def buildAssetList(coin_info):
+        pass
+    
     # FUNCTION: rankCoins
     # INPUT: TODO
     # OUTPUT: [(asset, value), ...]
@@ -54,20 +61,18 @@ class CoinRanker():
 
     # FUNCTION: assignValue
     # INPUT: tuple
+    # OUTPUT: int
     # DESCRIPTION:
     #   Takes an input coin tuple and assigns an aggregate score based on its values.
     def assignValue(coin_tuple):
 
-        # Ticker system is used to aid in determining value
         ticker = 0
         for category in coin_tuple:
             # First check the special case
             # - Potentially assign to deviate list
-
             # Otherwise,
             #   Do something to figure out the betsize
             #    Average each value with a weight towards some values(maybe some categories count twice for weighting)
-
         # Value might be (betsize, #)
         # where betsize is [small, medium, large]
         #   and # is some score/rank from 1-10
@@ -82,58 +87,6 @@ class CoinRanker():
 
         return something
 
-    # FUNCTION: main
-    # INPUT: assets - list of supported assets
-    # OUTPUT: N/A
-    # DESCRIPTION: 
-    #   Top level function for the coin ranker.
-    def main(assets):
-        # Balances should probably be a dictionary
-        balances = getBalances(asset)
-        coin_list = rankCoins()
-        distribution_values = allocateFunds(coin_list, balances)
-
-# CLASS: CoinBuyer
-class CoinBuyer():
-
-    # INPUT:  dictionary of [grouping : [pairing : percentage_allocated]
-    #         total value to be allocated
-    # 1. Check exchanges pairing is on
-    # 2. Allocate quantity based on input
-    # 3. Buy from exchange with API written
-    #   a. Return error if exchange API doesn't exist
-    # 4. Validate buy went through
-    # 5. Store result in database
-    # 6. Move onto next coin
-
-
-# TODO:
-# - Minimum buy all coins (? $2, .0005 btc minimum)
-# - Given list of coins, find all exchanges needed
-# - Plan exchange APIs
-# - First round buys all coins on exchanges that are already supported
-# - Function that outputs BTC quantity required, function that transfers necessary funds to buy
-#    all coins to appropiate exchanges
-# - Logic functions should deal with percentage of total input
-
-# ----------- COIN RANKER THEORY -----------
-# PART ONE:
-# Dictionary for each grouping:
-# OUTER DICTIONARY: [grouping : grouping_dict]
-# INNER DICTIONARY: [currency : aggregate_score]
-# FOR EACH GROUPING:
-#   Output aggregate score for a currency using logic functions
-#   FUNCTION(TUPLE)
-#       FUNCTION(PRICE) :: DO THIS TO SCORE
-#       FUNCTION(VOLUME) :: DO THIS TO SCORE (+1)
-#       RETURN AGGREGATE SCORE
-#   Add [currency : aggregate_score] to current grouping_dict
-
-# PART TWO:
-# INPUT: grouping dictionary from previous part
-# OUTER DICTIONARY: [grouping : value_dict]
-# INNER DICTIONARY: [currency : percentage_allocated]
-# FOR each grouping
-#   allocate certain percentage of fund based on grouping type
-#   allocate percentage of grouping_fund to each currency based on aggregate_score
-# Return outer dictionary to coinbuyer
+    # FUNCTION: buyCoin
+    def buyCoin():
+        pass
--- Bots/Coin-Categorizer/README.md
+++ Bots/Coin-Categorizer/README.md
@@ -1,14 +1,70 @@
 # Coin-Categorizer
-Description to come, for now just organizing todo and cleaning up current files.
+ Purpose:
+   Iterate through a list of cryptocurrencies with defined attributes, give
+   them an aggregate  score based on these attributes, identify coins whichare
+   'to be bought' and then go through
+   the API of whatever sites are necessary to buy them.
 
-* Clean up comments, port theory to appropiate file
-* Fix class structure
-* Write spreadsheetparser
-* Work on assignValue logic
-* Work on allocatedfunds function
-* Skeleton for buying hte coins
-* Port over API
-* Setup main script
-* Spreadsheet reading
-* Logic for classification
-* Coin list
+## Theory
+----------- COIN RANKER THEORY -----------
+PART ONE:
+ Dictionary for each grouping:
+ OUTER DICTIONARY: [grouping : grouping_dict]
+ INNER DICTIONARY: [currency : aggregate_score]
+ FOR EACH GROUPING:
+   Output aggregate score for a currency using logic functions
+   FUNCTION(TUPLE)
+       FUNCTION(PRICE) :: DO THIS TO SCORE
+       FUNCTION(VOLUME) :: DO THIS TO SCORE (+1)
+       RETURN AGGREGATE SCORE
+   Add [currency : aggregate_score] to current grouping_dict
+
+ PART TWO:
+ INPUT: grouping dictionary from previous part
+ OUTER DICTIONARY: [grouping : value_dict]
+ INNER DICTIONARY: [currency : percentage_allocated]
+ FOR each grouping
+   allocate certain percentage of fund based on grouping type
+   allocate percentage of grouping_fund to each currency based on aggregate_score
+ Return outer dictionary to coinbuyer
+ 
+## Coin Classification
+
+     [Asset :  (PRICE, VOLUME, MARKETS, TIMELINE, SOCIAL_NEWS, WEBSITE,
+                   SOURCE_CODE, PRICE_MOV, MINEABLE, WHITEPAPER)]  
+     [ETH   :   (8, 10, 8, ...)] --> 8
+ LENGTH = (1 + NUMBER OF VARIABLES)
+ 
+## Scoring System
+Notes here on how a currency will be ranked and given an aggregate score later
+
+## Allocation Method
+Notes here on how funds will be allocated based on score.
+
+## Acquiring Coins
+
+INPUT:  dictionary of [grouping : [pairing : percentage_allocated]
+        total value to be allocated
+1. Check exchanges pairing is on
+2. Allocate quantity based on input
+3. Buy from exchange with API written
+   a. Return error if exchange API doesn't exist
+4. Validate buy went through
+5. Store result in database
+6. Move onto next coin
+
+## TO-DO
+
+
+
+Last thing I was on was: rankCoins and organize below that.
+
+
+# TODO:
+# - Minimum buy all coins (? $2, .0005 btc minimum)
+# - Given list of coins, find all exchanges needed
+# - First round buys all coins on exchanges that are already supported
+# - Function that outputs BTC quantity required, function that transfers necessary funds to buy
+#    all coins to appropiate exchanges
+# - Logic functions should deal with percentage of total input
+
