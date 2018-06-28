cdfe263fa59de14ff105dd94537ec9c4e576437d
Carson Packer <carsongpacker@gmail.com>
2018-06-28 11:12:33 -0400

    Skeleton frontendtest

--- Tests/ComponentTests/FrontendTest.py
+++ Tests/ComponentTests/FrontendTest.py
@@ -1,2 +1,22 @@
-# TODO
+# FrontEndTest.py
+# Carson Packer
+# Tester
+# DESCRIPTION:
+#    Tester file for frontend. Builds a screen which allows one to cycle through test widgets,
+#     one must input a test widget to the tester class.
 
+# External-Imports
+#
+
+# Internal-Imports
+
+#
+class FrontEndTest(object):
+    pass
+    # TODO: init
+
+    # TODO: screen
+
+    # TODO: display widget
+
+    # TODO: swap button
