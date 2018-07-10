cb69dd7b18ced63b11ed7eef1b5e0aa1eabf6ba4
Carson Packer <carsongpacker@gmail.com>
2018-07-09 22:37:00 -0400

    Started to work in new generic top level initialization/runtime

--- Bots/System-Controller/BlueTitan.py
+++ Bots/System-Controller/BlueTitan.py
@@ -24,26 +24,12 @@ from FundAllocator import FundAllocator
 from PrintLibrary import PrintLibrary
 import Helpers
 from ArbitrageMain import ArbitrageTrader
+from GeneralizedDatabase import GenDatabaseLibrary
 
 class BlueTitan(object):
 
-	# INITIALIZATION: BlueTitan
-	# INPUT: algorithms - list of algorithms desired to be running, additionally contains
-	#                     information like acceptable exchanges and pairings to be run over.
-	# DESCRIPTION:
-	#    Performs initialization for BlueTitan trading system object
-	def __init__(self, algorithms, clean=True):
-		PrintLibrary.header("BlueTitan Initialization")
-
-		# Pull assets from pairing
-		assets = Helpers.convertPairingListAsset(pairings)
-		
-        PrintLibrary.displayVariables(exchanges, "Exchanges")
-        PrintLibrary.displayVariables(assets, "Assets")
-        PrintLibrary.displayVariables(pairings, "Pairings")
-
-		# 1. Database initialization
-        arbitrage_tables = ["AccountBalances", "BalancingHistory", "AssetInformation", "FailureTrades",
+	running_algos = []
+	        arbitrage_tables = ["AccountBalances", "BalancingHistory", "AssetInformation", "FailureTrades",
                             "IntendedFAE"]
          metrics_tables = ["Metrics", "FailureMetrics"]
          assetmetrics_tables = ["AssetMetrics", "AssetFailureMetrics"]
@@ -55,32 +41,56 @@ class BlueTitan(object):
         orig_exceptions = ["ArbitrageTrades", "Metrics", "AssetInformation", "FailureTrades", "BalancingHistory"]
         exceptions = ["ArbitrageTrades", "Metrics", "AssetInformation", "FailureTrades", "BalancingHistory"]
 
+	# INITIALIZATION: BlueTitan
+	# INPUT: algorithms - list of algorithms desired to be running, additionally contains
+	#                     information like acceptable exchanges and pairings to be run over.
+	# DESCRIPTION:
+	#    Performs initialization for BlueTitan trading system object
+	def __init__(self, algorithms, clean=True):
+		PrintLibrary.header("BlueTitan Initialization")
 
-        # Clean database [tables - exceptions]
-        if clean == True:
-            temporary solution for doing a complete database wipe when necessary
-            superclean = False
+		for algorithm in algorithms:
+			print(algorithm)
+			running_algos.append(algorithm)
 
-             # Messy, only works since it gets added to table that then gets instantly deleted. Figure out workaround 
-             #   for the future.
-             fae_list = BalancingLibrary.initializeFAEProportions(assets, exchanges, True)
+			# TODO: pull pairings
+			# TODO: pull assets
+			pairings = 0
+			assets = Helpers.convertPairingListAsset(pairings)
+			# TODO: pull exchanges
+			exchanges = 0
+		
+			PrintLibrary.displayVariables(exchanges, "Exchanges")
+			PrintLibrary.displayVariables(assets, "Assets")
+			PrintLibrary.displayVariables(pairings, "Pairings")
 
-            for database_tuple in databases:
-                PrintLibrary.displayVariables(database_tuple)
-                set_tables = set(database_tuple[2])
-                set_i_tables = set(DatabaseLibraryBase.listTablesDB(database_tuple[1]))
+			# TODO: How to know database table names and exceptions
+			
+			# Clean database [tables - exceptions]
+			if clean == True:
+				# temporary solution for doing a complete database wipe when necessary
+				superclean = False
 
-                print("Intended Tables: ", set_tables)
-                print("Database tables", set_i_tables)
-                PrintLibrary.delimiter()
-                if (set_tables.issubset(set_i_tables) and set_tables.issuperset(set_i_tables)) or superclean==True:
-                    exceptions = DatabaseLibraryBase.cleanDatabase(database_tuple[1], database_tuple[2])
-                else:
-                    exceptions = DatabaseLibraryBase.cleanDatabase(database_tuple[1], database_tuple[2], exceptions)
+				# Messy, only works since it gets added to table that then gets instantly deleted. Figure out workaround 
+				#   for the future.
+				fae_list = BalancingLibrary.initializeFAEProportions(assets, exchanges, True)
 
-                DatabaseLibrary.initialize(exceptions, assets, exchanges)
+				for database_tuple in databases:
+					PrintLibrary.displayVariables(database_tuple)
+					set_tables = set(database_tuple[2])
+					set_i_tables = set(DatabaseLibraryBase.listTablesDB(database_tuple[1]))
+
+					print("Intended Tables: ", set_tables)
+					print("Database tables", set_i_tables)
+					PrintLibrary.delimiter()
+					if (set_tables.issubset(set_i_tables) and set_tables.issuperset(set_i_tables)) or superclean==True:
+						exceptions = DatabaseLibraryBase.cleanDatabase(database_tuple[1], database_tuple[2])
+					else:
+						exceptions = DatabaseLibraryBase.cleanDatabase(database_tuple[1], database_tuple[2], exceptions)
+
+					DatabaseLibrary.initialize(exceptions, assets, exchanges)
 				
-        if "IntendedFAE" not in orig_exceptions:
+		if "IntendedFAE" not in orig_exceptions:
             BalancingLibrary.initializeFAE(assets, exchanges, class_balance_dict)
 
 		# 2. Initialize scheduling events
@@ -88,15 +98,19 @@ class BlueTitan(object):
         schedule.every().hour.do(ProfitTracker.runHourly, exchanges, assets)
         schedule.every().day.at("6:00").do(ProfitTracker.runDaily, exchanges, assets)
 
-		# 3. Run arbitrage algorithm
-		# NOTE: only running algorithm for now
-
-	
+	# MAIN: BlueTitan
+	# INPUT: N/A
+	# OUTPUT: N/A
+	# DESCRIPTION:
+	#    
 	def main():
-		pass
+		while 1:
+			# 1. Run Arbitrage Algorithm
+			# NOTE: only running algorithm for now
+		
 
 
 if __name__ == "__main__":
-	algorithms = []
-	
+	algorithms = {}
+	algorithm["Arbitrage"] = [("pairing1", ("exchange1", "exchange2"))]
 	testbt = BlueTitan().main()
